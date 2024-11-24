5385419067292f507495d09f2aed2d8da5c620ff
Garima888 <garimapatel951@gmail.com>
2024-11-24 22:32:49 +0530

    Add files via upload

---
+++ Hellofunction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void printHello(); // function declaraction/ prototype
+int main() {
+    printHello(); //function calling
+    printHello();//function calling
+    printHello();//function calling
+    return 0;
+}
+//function definition
+void printHello() {
+    printf("\nHello"); 
+    printf("\n * * * * *");
+    }
